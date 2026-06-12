0d1af85af1107084b975ad2eae6c3f55f017f08e
AgathaVAlmeida <victorianonatodealmeidaagatha@gmail.com>
2026-06-12 09:51:56 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -51,6 +51,53 @@ atualizarContador(
         seg: "segSoja"
     }
 );
+    function atualizarTodos() {
+
+// Próximo plantio da soja
+atualizarContador(
+    new Date("2026-09-01T00:00:00"),
+    {
+        dias: "diasSoja",
+        horas: "horasSoja",
+        min: "minSoja",
+        seg: "segSoja"
+    }
+);
+
+// Meta tecnológica
+atualizarContador(
+    new Date("2030-01-01T00:00:00"),
+    {
+        dias: "diasTec",
+        horas: "horasTec",
+        min: "minTec",
+        seg: "segTec"
+    }
+);
+
+// Dia Mundial do Meio Ambiente
+atualizarContador(
+    new Date("2027-06-05T00:00:00"),
+    {
+        dias: "diasAmb",
+        horas: "horasAmb",
+        min: "minAmb",
+        seg: "segAmb"
+    }
+);
+
+// Agenda 2030
+atualizarContador(
+    new Date("2030-12-31T23:59:59"),
+    {
+        dias: "dias2030",
+        horas: "horas2030",
+        min: "min2030",
+        seg: "seg2030"
+    }
+);
+
+    }
 
 }
 
